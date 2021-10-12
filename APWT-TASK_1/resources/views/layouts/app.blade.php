02e6b9a5a525d7bdfcdf3617c055f9d588575e7a
MD Tanjimul Islam <67940292+tnr-tanjimul@users.noreply.github.com>
2021-10-13 01:28:36 +0600

    Create app.blade.php

---
+++ APWT-TASK_1/resources/views/layouts/app.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Title -->
+    <title>TNRSOFT || A Professional Android App Development Company</title>
+    
+    <!-- Favicon -->
+    
+
+    @include('inc.styles')
+
+</head>   
+    
+<body>
+    
+    
+    
+    
+    <!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v5.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your customer chat code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="223557268053052"
+  theme_color="#44bec7">
+      </div>  
+	  
+	  
+    
+
+    
+   
+    
+         
+   
+
+@yield('content')    
+
+@include('inc.footer')
+    
+
+</body>
+</html>
